0de89e745f5a4ba8316377a0c3d707a0cd9f39ef
Dylan Katz <muddmaker.97@gmail.com>
2018-05-14 17:05:03 -0700

    Refactor message out of sendf

--- bot.py
+++ bot.py
@@ -39,12 +39,12 @@ def getip(url):
     except socket.gaierror:
         return False
 
-async def sendf(msg, fmt, *args):
+async def sendf(chn, fmt, *args):
     '''
     Helper method to send a formatted message to a the same channel as the
     previous channel
     '''
-    await CLIENT.send_message(msg.channel, fmt.format(*args))
+    await CLIENT.send_message(chn, fmt.format(*args))
 
 async def dispatch(message):
     '''
@@ -52,37 +52,38 @@ async def dispatch(message):
     the actual system commands that get run.
     '''
     data = message.content.split(' ')
+    chn = message.channel
 
     if data[0] not in CMD_ARGS:
         return
 
     if len(data) - 1 != CMD_ARGS[data[0]]:
-        await sendf(message, 'Error: `{}` takes {} argument(s)', data[0], CMD_ARGS[data[0]])
+        await sendf(chn, 'Error: `{}` takes {} argument(s)', data[0], CMD_ARGS[data[0]])
         return
 
     if data[0] == '!hello':
-        await sendf(message, 'Hello {}', message.author.mention)
+        await sendf(chn, 'Hello {}', message.author.mention)
     elif data[0] == '!ping':
-        await sendf(message, 'Pinging {}', data[1])
+        await sendf(chn, 'Pinging {}', data[1])
         msg = ping(data[1])
 
         if not msg:
             msg = 'Host not found / up'
 
-        await sendf(message, msg)
+        await sendf(chn, msg)
     elif data[0] == '!ip':
-        await sendf(message, 'Ip Address: {}', getip(data[1]))
+        await sendf(chn, 'Ip Address: {}', getip(data[1]))
     elif data[0] == '!geo':
         real_ip = getip(data[1])
 
         if not real_ip:
-            await sendf(message, 'Host not found / up')
+            await sendf(chn, 'Host not found / up')
             return
 
-        await sendf(message, 'Ip Address: {}', real_ip)
+        await sendf(chn, 'Ip Address: {}', real_ip)
         req = requests.get('http://ipinfo.io/{}?token={}'.format(real_ip, IPINFO_TOKEN))
         jdata = json.loads(req.text)
-        await sendf(message, 'https://maps.google.com?q={}', jdata['loc'])
+        await sendf(chn, 'https://maps.google.com?q={}', jdata['loc'])
 
 @CLIENT.event
 async def on_message(message):
